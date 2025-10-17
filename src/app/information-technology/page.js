0b0d24e262201b4e2da6f5f50ab87ebf11ffa2b2
vivekmandiya-sevenmentor <vmandiya@sevenmentor.services>
2025-10-17 15:57:38 +0530

    changes in my branch

---
+++ src/app/information-technology/page.js
@@ -0,0 +1,10 @@
+import InformationTechnology from '@/components/InformationTechnology/InformationTechnology'
+import React from 'react'
+
+function page() {
+  return (
+    <><InformationTechnology/></>
+  )
+}
+
+export default page

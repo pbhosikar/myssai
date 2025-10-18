2fe45dcca80d0b11c85850d5f6d459717693557e
Gopal Thakur <gopal.thakur@ssaihq.com>
2025-10-18 21:00:11 +0530

    g

--- src/components/pages/CompanyView.jsx
+++ src/components/pages/CompanyView.jsx
@@ -127,7 +127,6 @@ export default function CompanyView() {
                 </div>
               </aside>
             </div>
-            </div>
             
           </section>
         </div>

6c8d9844a96767c4522c277b7042417f04141794
Gopal Thakur <gopal.thakur@ssaihq.com>
2025-10-24 23:11:21 +0530

    added ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,9 +8,10 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
         NEXTAUTH_URL: 'https://mystage.ssai.app',
-        NEXTAUTH_SECRET: 'some-random-secret',
-        GOOGLE_CLIENT_ID: 'your_google_client_id',
-        GOOGLE_CLIENT_SECRET: 'your_google_client_secret',
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        AZURE_AD_CLIENT_ID: process.env.AZURE_AD_CLIENT_ID,
+        AZURE_AD_CLIENT_SECRET: process.env.AZURE_AD_CLIENT_SECRET,
+        AZURE_AD_TENANT_ID: process.env.AZURE_AD_TENANT_ID,
         PORT: 3031,
       },
     },
